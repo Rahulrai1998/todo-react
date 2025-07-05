79a0036d7bdb4685e88593ac58859a5b03ccd0dc
Rahul Kumar <rahulrai2822march@gmail.com>
2025-07-05 22:53:48 +0530

    ref used

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { IoIosAddCircle } from "react-icons/io";
 
 const AddTodo = ({ handleAddTodo }) => {
-  const [todo, setTodo] = useState({ id: 1, text: "", date: "" });
+  const textRef = useRef("");
+  const dateRef = useRef("");
+  // const [todo, setTodo] = useState({ id: 1, text: "", date: "" });
 
-  const handleTodoChange = (event) => {
-    setTodo({ ...todo, text: event.target.value });
-  };
-  const handleTimeChange = (event) => {
-    setTodo({ ...todo, date: event.target.value });
-  };
+  // const handleTodoChange = (event) => {
+  //   setTodo({ ...todo, text: event.target.value });
+  // };
+  // const handleTimeChange = (event) => {
+  //   setTodo({ ...todo, date: event.target.value });
+  // };
 
   const handleAddClick = () => {
-    handleAddTodo(todo);
-    setTodo({ id: 1, text: "", date: "" });
+    handleAddTodo({
+      id: 1,
+      text: textRef.current.value,
+      date: dateRef.current.value,
+    });
+    textRef.current.value = "";
+    dateRef.current.value = "";
   };
 
   return (
@@ -21,19 +28,21 @@ const AddTodo = ({ handleAddTodo }) => {
       <div className="row custom-row d-flex justify-content-end gap-0">
         <div className="col-4">
           <input
-            value={todo.text}
+            ref={textRef}
+            // value={todo.text}
             type="text"
             placeholder="Enter here..."
             className="form-control"
-            onChange={handleTodoChange}
+            // onChange={handleTodoChange}
           />
         </div>
         <div className="col-4">
           <input
+            ref={dateRef}
             type="date"
             className="form-control"
-            value={todo.date}
-            onChange={handleTimeChange}
+            // value={todo.date}
+            // onChange={handleTimeChange}
           />
         </div>
         <div className="col-2">
